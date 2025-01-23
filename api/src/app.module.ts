8b92c9b0990c7d00761470650b9e571f2eebfd5c
Renzxs <florencebatol85@gmail.com>
2025-01-24 00:03:14 +0800

    Added DTOs and Schemas

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
+  imports: [ 
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+    }),
+    MongooseModule.forRoot('mongodb://localhost:27017'),
+    ProductsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ api/src/products/schemas/product.schema.ts
@@ -0,0 +1,14 @@
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class Product extends Document {
+    @Prop({ required: true })
+    name: string;
+
+    @Prop()
+    description: string;
+
+    @Prop({ required: true })
+    price: number;
+}

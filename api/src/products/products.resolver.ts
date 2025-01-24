cc9f925238cb049c842ce3037c4e0ae955e4a93c
Renzxs <florencebatol85@gmail.com>
2025-01-24 16:31:11 +0800

    Completed Product API

--- api/src/products/products.resolver.ts
+++ api/src/products/products.resolver.ts
@@ -1,4 +1,35 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver, Query } from '@nestjs/graphql';
+import { Product } from './entities/product.entity';
+import { ProductsService } from './products.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
 
-@Resolver()
-export class ProductsResolver {}
+@Resolver(() =>  Product)
+export class ProductsResolver {
+    constructor(private readonly productsService: ProductsService) {}
+    
+    @Mutation(() => Product)
+    createProduct(@Args('createProductDto') createProductDto: CreateProductDto) {
+        return this.productsService.create(createProductDto);
+    }
+
+    @Query(() => [Product], { name: 'products'})
+    findAll() {
+        return this.productsService.findAll();
+    }
+
+    @Query(() => Product, { name: 'product' })
+    findOne(@Args('id') id: string) {
+        return this.productsService.findOne(id);
+    }
+
+    @Mutation(() => Product)
+    updateProduct(@Args('updateProductDto') updateProductDto: UpdateProductDto) {
+        return this.productsService.update(updateProductDto.id, updateProductDto);
+    }
+
+    @Mutation(() => Boolean)
+    deleteProduct(@Args('id') id: string) {
+        return this.productsService.delete(id);
+    }
+}
--- api/src/products/products.service.ts
+++ api/src/products/products.service.ts
@@ -3,6 +3,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Product } from './schemas/product.schema';
 import { Model } from 'mongoose';
 import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
 
 @Injectable()
 export class ProductsService {
@@ -13,5 +14,20 @@ export class ProductsService {
         return product.save();
     }
 
+    async findAll(): Promise<Product[]> {
+        return this.productModel.find().exec();
+    }
 
-}
+    async findOne(id: string): Promise<Product | null> {
+        return this.productModel.findById(id).exec();
+    }
+
+    async update(id: string, updateProductDto: UpdateProductDto): Promise<Product | null> {
+        return this.productModel.findByIdAndUpdate(id, updateProductDto, { new: true }).exec();
+    }
+
+    async delete(id: string): Promise<boolean> {
+        const result = this.productModel.findByIdAndDelete(id).exec();
+        return !!result;
+    }
+}   
